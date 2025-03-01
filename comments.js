0024227ca071312c9a593c2eed4ea6c43c0a1308
Srujana-dev123 <dachepallisrujana@gmail.com>
2025-03-01 18:00:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,85 +1,30 @@
 //create web server
 var express = require('express');
 var app = express();
-//create server
-var http = require('http').Server(app);
-//create socket
-var io = require('socket.io')(http);
-//create fs
-var fs = require('fs');
-//create path
-var path = require('path');
-//create body-parser
-var bodyParser = require('body-parser');
-//create cookie-parser
-var cookieParser = require('cookie-parser');
-//create session
-var session = require('express-session');
-//create morgan
-var morgan = require('morgan');
-//create method-override
-var methodOverride = require('method-override');
-//create errorhandler
-var errorHandler = require('errorhandler');
-//create multer
-var multer = require('multer');
-//create mongoose
-var mongoose = require('mongoose');
-//create Schema
-var Schema = mongoose.Schema;
-//create assert
-var assert = require('assert');
-//create passport
-var passport = require('passport');
-//create LocalStrategy
-var LocalStrategy = require('passport-local').Strategy;
-//create flash
-var flash = require('connect-flash');
-//create crypto
-var crypto = require('crypto');
-//create nodemailer
-var nodemailer = require('nodemailer');
-//create smtpTransport
-var smtpTransport = require('nodemailer-smtp-transport');
-//create moment
-var moment = require('moment');
-//create async
-var async = require('async');
-//create crypto
-var crypto = require('crypto');
-//create bcrypt
-var bcrypt = require('bcrypt-nodejs');
-//create users
-var users = require('./users');
-//create comments
-var comments = require('./comments');
-//create config
-var config = require('./config');
-//create user
-var User = require('./models/user');
-//create comment
-var Comment = require('./models/comment');
-//create user
-var user = new User();
-//create comment
-var comment = new Comment();
-//create port
-var port = process.env.PORT || 3000;
-//create db
-var db = mongoose.connection;
-//create db
-var db = mongoose.connection;
-//create db
-var db = mongoose.connection;
-//connect to db
-mongoose.connect('mongodb://localhost:27017/comments');
-//create Schema
-var Schema = mongoose.Schema;
-//create new Schema
-var commentSchema = new Schema({
-    user: String,
-    content: String,
-    created: Date
+
+//create a comments object
+var comments = {
+  "1": "This is the first comment",
+  "2": "This is the second comment",
+  "3": "This is the third comment"
+};
+
+//create a route to get all comments
+app.get('/comments', function(req, res) {
+  res.send(comments);
 });
-//create model
-var Comment = mongoose.model('Comment', commentSchema);
+
+//create a route to get a comment by id
+app.get('/comments/:id', function(req, res) {
+  var comment = comments[req.params.id];
+  if (comment) {
+    res.send(comment);
+  } else {
+    res.status(404).send("Comment not found");
+  }
+});
+
+//start the server
+var server = app.listen(3000, function() {
+  console.log('Server listening on port 3000');
+});
