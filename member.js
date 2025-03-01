384a1f969e08bba045df9b2f43fdbcd265f6ddfd
Srujana-dev123 <dachepallisrujana@gmail.com>
2025-03-01 17:20:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 32,
+        skills: ['HTML', 'CSS', 'JS'],
+        'full name': 'John Doe'
+    };
+    return member;
+}
